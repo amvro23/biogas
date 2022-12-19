af8f4ebb462a165b1e1bf035ace53c30db6ac1b6
Amvro23 <91277572+amvro23@users.noreply.github.com>
2022-12-19 17:27:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 
 setup(
-   name='Biogas_reforming',
+   name='biogas',
    version='0.1.0.rc2',
    description='Biogas Reforming package.',
    long_description=long_description,
    author='Amvrosios Georgiadis',
    author_email='amvro23@gmail.com',
-   packages=['Biogas_reforming'],
+   packages=['biogas'],
    install_requires=[
       'numpy==1.20.*',
       'scipy>=1.7.*',
